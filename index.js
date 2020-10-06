ada98b746e5040015899c8e680fc16dcb903c153
Angela Conde <56882332+angelaconde@users.noreply.github.com>
2020-10-06 10:19:01 +0200

    Added GET and POST

--- index.js
+++ index.js
@@ -4,11 +4,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const MongoClient = require('mongodb').MongoClient;
 
-console.log('NODE WORKING');
-
-app.listen(3000, function () {
-    console.log('LISTENING ON 3000');
-});
+app.listen(3000, function () { });
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -18,12 +14,21 @@ const mongoString = 'mongodb+srv://aconde:BDMongoAtlas@cluster0.vo3xv.mongodb.ne
 MongoClient.connect(mongoString, { useUnifiedTopology: true }, (err, client) => {
     if (err) return console.error(err);
     console.log('Connected to Database');
-    const db = client.db('backlog');
-    const gamesCollection = db.collection('backlog');
-    app.get('/api/x', (req, res) => {
-        // TODO GET
+    const db = client.db('guestbook');
+    const gBookCollection = db.collection('guestbook');
+    app.get('/api/guestbook', (req, res) => {
+        db.collection('guestbook').find().toArray()
+            .then(results => {
+                res.send(results);
+            })
+            .catch(error => console.error(error));
     });
-    app.post('/api/x', (req, res) => {
-        // TODO POST
-    });
+    app.post('/api/guestbook', (req, res) => {
+        gBookCollection.insertOne(req.body)
+            .then(result => {
+                res.send(true);
+                console.log(req.body);
+            })
+            .catch(error => console.error(error));
+    })
 });
